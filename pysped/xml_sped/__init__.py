7c624898166a48038ae7c6fc1910a1a900d27785
Aristides Caldeira <aristides.caldeira@taugars.com.br>
2010-12-23 09:58:44 -0200

    Incluída a classe TagBoolean

--- pysped/xml_sped/__init__.py
+++ pysped/xml_sped/__init__.py
@@ -2,7 +2,7 @@
 
 from base import ABERTURA, NAMESPACE_NFE, NAMESPACE_SIG, \
     TagCaracter, TagData, TagDataHora, TagDecimal, TagHora, TagInteiro, \
-    XMLNFe, tira_abertura, tirar_acentos, por_acentos
+    XMLNFe, tira_abertura, tirar_acentos, por_acentos, TagBoolean
 
 
 from assinatura import Signature
