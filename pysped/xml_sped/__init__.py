98da6bf2132518ae3cdcd5cc716d5af9147987c1
Aristides Caldeira <aristides.caldeira@tauga.com.br>
2012-08-22 12:11:08 -0300

    Revert "Pequena alteração esquecida do commit anterior"
    This reverts commit 8c3cc40480785191a6b4c50baa46fd28c16bff34.

--- pysped/xml_sped/__init__.py
+++ pysped/xml_sped/__init__.py
@@ -42,7 +42,6 @@
 
 from base import ABERTURA, NAMESPACE_NFE, NAMESPACE_SIG, \
     TagCaracter, TagData, TagDataHora, TagDecimal, TagHora, TagInteiro, \
-    TagDataHoraUTC, \
     XMLNFe, tira_abertura, tirar_acentos, por_acentos, TagBoolean, somente_ascii
 
 
