97feb10459da637f31a64bd4e34737d52c92a8c4
Aristides Caldeira <aristides.caldeira@tauga.com.br>
2013-03-13 17:59:25 -0300

    Faltou este arquivo no commit anterior

--- pysped/xml_sped/cadeia-certificadora/converte-certificado.sh
+++ pysped/xml_sped/cadeia-certificadora/converte-certificado.sh
@@ -1,1 +1,3 @@
-openssl pkcs7 -inform DER -in mfirefox-der.der -out cadeia.pem -print_certs
+openssl pkcs7 -inform DER -in v1_ff.der -out cadeia-1.pem -print_certs
+openssl pkcs7 -inform DER -in v2_ff.der -out cadeia-2.pem -print_certs
+openssl pkcs7 -inform DER -in v3_ff.der -out cadeia-3.pem -print_certs
