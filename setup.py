fe7b0d8b9fc1102031a8b0bff75931e556f826fc
Johan Dahlin <johan@gnome.org>
2013-06-28 16:57:44 -0300

    setup: Correct license classifier

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Library General Public License v3 or later (AGPLv3+)',
+        'License :: OSI Approved :: GNU Library General Public License v3 or later (GPLv2.1+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
