283a871ebc8836774a5d7ecd6d8dcc20901a865c
Ari Caldeira <ari@erpintegra.com.br>
2014-09-25 10:54:49 -0300

    Ajustando leitura do IPI

--- pysped/nfe/leiaute/nfe_110.py
+++ pysped/nfe/leiaute/nfe_110.py
@@ -800,8 +800,9 @@ class IPI(XMLNFe):
 
 
     def get_xml(self):
-        if not ((self.CST.valor in ('00', '49', '50', '99')) or
-           (self.qUnid.valor or self.vUnid.valor or self.vBC.valor or self.pIPI.valor or self.vIPI.valor)):
+        if (self.CST.valor not in ('01', '02', '03', '04', '51', '52', '53', '54', '55')) or \
+           ((self.CST.valor in ('00', '49', '50', '99')) and \
+           not (self.qUnid.valor or self.vUnid.valor or self.vBC.valor or self.pIPI.valor or self.vIPI.valor)):
             return ''
 
         #
