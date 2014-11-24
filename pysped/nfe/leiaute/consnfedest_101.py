bf074c565483e84ababd3f4566e21062525b54d1
Ari Caldeira <ari@erpintegra.com.br>
2014-11-24 17:00:01 -0200

    Tratando a leitura do resultado da consulta de NF-e destinada

--- pysped/nfe/leiaute/consnfedest_101.py
+++ pysped/nfe/leiaute/consnfedest_101.py
@@ -42,7 +42,8 @@
 from __future__ import division, print_function, unicode_literals
 
 from pysped.xml_sped import (ABERTURA, NAMESPACE_NFE, TagCaracter,
-                             TagDataHora, TagDecimal, TagInteiro, XMLNFe)
+                             TagDataHora, TagDecimal, TagInteiro, XMLNFe,
+                             TagData, TagDataHoraUTC)
 from pysped.nfe.leiaute import ESQUEMA_ATUAL_VERSAO_2 as ESQUEMA_ATUAL
 import os
 
@@ -96,10 +97,123 @@ class ConsNFeDest(XMLNFe):
     xml = property(get_xml, set_xml)
 
 
+
+class ResNFe(XMLNFe):
+    def __init__(self):
+        super(ResNFe, self).__init__()
+        self.NSU      = TagCaracter(nome='resNFe', propriedade='NSU', raiz='/')
+        self.chNFe    = TagCaracter(nome='chNFe' , tamanho=[44, 44], raiz='//resNFe')
+        self.CNPJ     = TagCaracter(nome='CNPJ'  , tamanho=[14, 14], raiz='//resNFe', obrigatorio=False)
+        self.CPF      = TagCaracter(nome='CPF'   , tamanho=[11, 11], raiz='//resNFe', obrigatorio=False)
+        self.xNome    = TagCaracter(nome='xNome' , tamanho=[ 1, 60], raiz='//resNFe')
+        self.IE       = TagCaracter(nome='IE'    , tamanho=[ 2, 14], raiz='//resNFe', obrigatorio=False)
+        self.dEmi     = TagData(nome='dEmi'      ,                   raiz='//resNFe', obrigatorio=False)
+        self.tpNF     = TagCaracter(nome='tpNF'  , tamanho=[ 1,  1], raiz='//resNFe')
+        self.vNF      = TagDecimal(nome='vNF'    , tamanho=[1, 15, 1], decimais=[0, 2, 2], raiz='//resNFe')
+        self.digVal   = TagCaracter(nome='digVal', tamanho=[28, 28], raiz='//resNFe')
+        self.dhRecbto = TagDataHoraUTC(nome='dhRecbto', raiz='//resNFe')
+        self.cSitNFe  = TagCaracter(nome='cSitNFe', tamanho=[ 1,  1], raiz='//resNFe')
+        self.cSitConf = TagCaracter(nome='cSitConf', tamanho=[ 1,  1], raiz='//resNFe')
+
+    def get_xml(self):
+        xml = XMLNFe.get_xml(self)
+        xml += '<ret>'
+        xml += self.NSU.xml
+        xml += self.chNFe.xml
+        xml += self.CNPJ.xml
+        xml += self.CPF.xml
+        xml += self.xNome.xml
+        xml += self.IE.xml
+        xml += self.dEmi.xml
+        xml += self.tpNF.xml
+        xml += self.vNF.xml
+        xml += self.digVal.xml
+        xml += self.dhRecbto.xml
+        xml += self.cSitNFe.xml
+        xml += self.cSitConf.xml
+        xml += '</resNFe>'
+        xml += '</ret>'
+        return xml
+
+    def set_xml(self, arquivo):
+        if self._le_xml(arquivo):
+            self.NSU.xml   = arquivo
+            self.chNFe.xml   = arquivo
+            self.CNPJ.xml   = arquivo
+            self.CPF.xml   = arquivo
+            self.xNome.xml   = arquivo
+            self.IE.xml   = arquivo
+            self.dEmi.xml   = arquivo
+            self.tpNF.xml   = arquivo
+            self.vNF.xml   = arquivo
+            self.digVal.xml   = arquivo
+            self.dhRecbto.xml   = arquivo
+            self.cSitNFe.xml   = arquivo
+            self.cSitConf.xml   = arquivo
+
+    xml = property(get_xml, set_xml)
+
+
+class ResCanc(XMLNFe):
+    def __init__(self):
+        super(ResCanc, self).__init__()
+        self.NSU      = TagCaracter(nome='resCanc', propriedade='NSU', raiz='/')
+        self.chNFe    = TagCaracter(nome='chNFe' , tamanho=[44, 44], raiz='//resCanc')
+        self.CNPJ     = TagCaracter(nome='CNPJ'  , tamanho=[14, 14], raiz='//resCanc', obrigatorio=False)
+        self.CPF      = TagCaracter(nome='CPF'   , tamanho=[11, 11], raiz='//resCanc', obrigatorio=False)
+        self.xNome    = TagCaracter(nome='xNome' , tamanho=[ 1, 60], raiz='//resCanc')
+        self.IE       = TagCaracter(nome='IE'    , tamanho=[ 2, 14], raiz='//resCanc', obrigatorio=False)
+        self.dEmi     = TagData(nome='dEmi'      ,                   raiz='//resCanc', obrigatorio=False)
+        self.tpNF     = TagCaracter(nome='tpNF'  , tamanho=[ 1,  1], raiz='//resCanc')
+        self.vNF      = TagDecimal(nome='vNF'    , tamanho=[1, 15, 1], decimais=[0, 2, 2], raiz='//resCanc')
+        self.digVal   = TagCaracter(nome='digVal', tamanho=[28, 28], raiz='//resCanc')
+        self.dhRecbto = TagDataHoraUTC(nome='dhRecbto', raiz='//resCanc')
+        self.cSitNFe  = TagCaracter(nome='cSitNFe', tamanho=[ 1,  1], raiz='//resCanc')
+        self.cSitConf = TagCaracter(nome='cSitConf', tamanho=[ 1,  1], raiz='//resCanc')
+
+    def get_xml(self):
+        xml = XMLNFe.get_xml(self)
+        xml += '<ret>'
+        xml += self.NSU.xml
+        xml += self.chNFe.xml
+        xml += self.CNPJ.xml
+        xml += self.CPF.xml
+        xml += self.xNome.xml
+        xml += self.IE.xml
+        xml += self.dEmi.xml
+        xml += self.tpNF.xml
+        xml += self.vNF.xml
+        xml += self.digVal.xml
+        xml += self.dhRecbto.xml
+        xml += self.cSitNFe.xml
+        xml += self.cSitConf.xml
+        xml += '</resCanc>'
+        xml += '</ret>'
+        return xml
+
+    def set_xml(self, arquivo):
+        if self._le_xml(arquivo):
+            self.NSU.xml   = arquivo
+            self.chNFe.xml   = arquivo
+            self.CNPJ.xml   = arquivo
+            self.CPF.xml   = arquivo
+            self.xNome.xml   = arquivo
+            self.IE.xml   = arquivo
+            self.dEmi.xml   = arquivo
+            self.tpNF.xml   = arquivo
+            self.vNF.xml   = arquivo
+            self.digVal.xml   = arquivo
+            self.dhRecbto.xml   = arquivo
+            self.cSitNFe.xml   = arquivo
+            self.cSitConf.xml   = arquivo
+
+    xml = property(get_xml, set_xml)
+
+
 class RetConsNFeDest(XMLNFe):
     def __init__(self):
         super(RetConsNFeDest, self).__init__()
-        self.versao = TagDecimal(nome='retConsNFeDest', codigo='IR01', propriedade='versao', namespace=NAMESPACE_NFE, valor='1.01', raiz='/')
+        self.versao   = TagDecimal(nome='retConsNFeDest', codigo='IR01', propriedade='versao', namespace=NAMESPACE_NFE, valor='1.01', raiz='/')
         self.tpAmb    = TagInteiro(nome='tpAmb'         , codigo='IR03', tamanho=[1,   1, 1], raiz='//retConsNFeDest')
         self.verAplic = TagCaracter(nome='verAplic'     , codigo='IR04', tamanho=[1,  20]   , raiz='//retConsNFeDest')
         self.cStat    = TagCaracter(nome='cStat'        , codigo='IR05', tamanho=[1,   3]   , raiz='//retConsNFeDest')
@@ -107,6 +221,10 @@ class RetConsNFeDest(XMLNFe):
         self.dhResp   = TagDataHora(nome='dhResp'       , codigo='IR07',                      raiz='//retConsNFeDest')
         self.indCont  = TagCaracter(nome='indCont'      , codigo='IR08', tamanho=[1,   1, 1], raiz='//retConsNFeDest', obrigatorio=False)
         self.ultNSU   = TagCaracter(nome='ultNSU'       , codigo='IP09', tamanho=[1, 15]    , raiz='//retConsNFeDest', obrigatorio=False)
+        self.resNFe   = []
+        self.resCanc  = []
+        self.resCCe   = []
+
         self.caminho_esquema = os.path.join(DIRNAME, 'schema', ESQUEMA_ATUAL + '/')
         self.arquivo_esquema = 'retconsNFeDest_v1.01.xsd'
 
@@ -122,6 +240,15 @@ class RetConsNFeDest(XMLNFe):
         xml += self.indCont.xml
         xml += self.ultNSU.xml
 
+        for n in self.resNFe:
+            xml += n.xml
+
+        for n in self.resCanc:
+            xml += n.xml
+
+        for n in self.resCCe:
+            xml += n.xml
+
         xml += '</retConsNFeDest>'
         return xml
 
@@ -135,5 +262,15 @@ class RetConsNFeDest(XMLNFe):
             self.dhResp.xml   = arquivo
             self.indCont.xml   = arquivo
             self.ultNSU.xml   = arquivo
+
+            #
+            # Técnica para leitura de tags múltiplas
+            # As classes dessas tags, e suas filhas, devem ser
+            # "reenraizadas" (propriedade raiz) para poderem ser
+            # lidas corretamente
+            #
+            self.resNFe = self.le_grupo('//ret/resNFe', ResNFe)
+            self.resCanc = self.le_grupo('//ret/resCanc', ResCanc)
+            #self.resCCe = self.le_grupo('//ret/resCCe', ResCCe)
 
     xml = property(get_xml, set_xml)
