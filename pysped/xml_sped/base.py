87559883bf8e16d75589092404b3c25461acc2ba
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-06-11 10:51:25 -0300

    Correção de urls de consulta distribuição e consulta cadastro

--- pysped/xml_sped/base.py
+++ pysped/xml_sped/base.py
@@ -429,7 +429,7 @@ class TagData(TagCaracter):
     def set_valor(self, novo_valor):
         if isinstance(novo_valor, basestring):
             if novo_valor:
-                novo_valor = datetime.strptime(novo_valor, '%Y-%m-%d')
+                novo_valor = datetime.strptime(novo_valor[:10], '%Y-%m-%d')
             else:
                 novo_valor = None
 
