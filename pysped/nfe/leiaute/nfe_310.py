ee3087de3691a1f8a32e9bae58b52421c5a1f9f9
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-01-12 17:49:46 -0200

    Corrigindo tratamento da NFC-e

--- pysped/nfe/leiaute/nfe_310.py
+++ pysped/nfe/leiaute/nfe_310.py
@@ -185,11 +185,11 @@ class ICMSUFDest(XMLNFe):
         self.pICMSUFDest = TagDecimal(nome='pICMSUFDest', codigo='NA07', tamanho=[1,  5, 1], decimais=[0, 4, 4], raiz='//det/imposto/ICMSUFDest')
         self.pICMSInter = TagDecimal(nome='pICMSInter', codigo='NA09', tamanho=[1,  5, 1], decimais=[0, 2, 2], raiz='//det/imposto/ICMSUFDest')
         self.pICMSInterPart = TagDecimal(nome='pICMSInterPart', codigo='NA11', tamanho=[1,  5, 1], decimais=[0, 4, 4], raiz='//det/imposto/ICMSUFDest', valor=40)
-        
+
         self.vFCPUFDest = TagDecimal(nome='vFCPUFDest', codigo='NA13', tamanho=[1, 15, 1], decimais=[0, 2, 2], raiz='//det/imposto/ICMSUFDest')
         self.vICMSUFDest = TagDecimal(nome='vICMSUFDest', codigo='N15', tamanho=[1, 15, 1], decimais=[0, 2, 2], raiz='//det/imposto/ICMSUFDest')
         self.vICMSUFRemet = TagDecimal(nome='vICMSUFRemet', codigo='N15', tamanho=[1, 15, 1], decimais=[0, 2, 2], raiz='//det/imposto/ICMSUFDest')
-        
+
 
     def get_xml(self):
         if not (self.vBCUFDest.valor):
@@ -1286,6 +1286,26 @@ class Pag(XMLNFe):
 
     xml = property(get_xml, set_xml)
 
+    @property
+    def pagamento_formatado(self):
+        TIPOS = {
+            '01': 'Dinheiro',
+            '02': 'Cheque',
+            '03': 'Cartão de crédito',
+            '04': 'Cartão de débito',
+            '05': 'Crédito loja',
+            '10': 'Vale alimentação',
+            '11': 'Vale refeição',
+            '12': 'Vale presente',
+            '13': 'Vale combustível',
+            '99': 'Outros',
+        }
+
+        if self.tPag.valor not in TIPOS:
+            return ''
+
+        return TIPOS[self.tPag.valor]
+
 
 class Dup(nfe_200.Dup):
     def __init__(self):
@@ -1600,11 +1620,16 @@ class Dest(nfe_200.Dest):
 
         xml += self.enderDest.xml
 
-        if self.indIEDest.valor:
-            xml += self.indIEDest.xml
+        if self.modelo == '55':
+            if self.indIEDest.valor:
+                xml += self.indIEDest.xml
 
-        if (not self.idEstrangeiro.valor) or (self.indIEDest.valor != '2' and self.IE.valor):
-            xml += self.IE.xml
+            if (not self.idEstrangeiro.valor) or (self.indIEDest.valor != '2' and self.IE.valor):
+                xml += self.IE.xml
+
+        else:
+            self.indIEDest.valor = '9'
+            xml += self.indIEDest.xml
 
         xml += self.ISUF.xml
         xml += self.IM.xml
@@ -1705,7 +1730,8 @@ class Ide(nfe_200.Ide):
 
         self.dEmi.valor = self.dhEmi.valor
 
-        xml += self.dhSaiEnt.xml
+        if self.mod.valor == '55':
+            xml += self.dhSaiEnt.xml
 
         self.dSaiEnt.valor = self.dhSaiEnt.valor
         self.hSaiEnt.valor = self.hSaiEnt.valor
@@ -1820,7 +1846,9 @@ class InfNFe(nfe_200.InfNFe):
 
         xml += self.total.xml
         xml += self.transp.xml
-        xml += self.cobr.xml
+
+        if self.ide.mod.valor == '55':
+            xml += self.cobr.xml
 
         if self.ide.mod.valor == '65':
             for p in self.pag:
