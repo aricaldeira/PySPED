1f09bc96705e17a5107a7e4a4e20b68cf0a9d3d5
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-02-21 17:10:07 -0300

    Corrigindo CST permitida para o IPI

--- pysped/nfe/leiaute/nfe_310.py
+++ pysped/nfe/leiaute/nfe_310.py
@@ -438,6 +438,9 @@ class IPI(nfe_200.IPI):
         self.pIPI = TagDecimal(nome='pIPI', codigo='O13', tamanho=[1,  5, 1], decimais=[0, 4, 4], raiz='')
 
     def get_xml(self):
+        if self.CST.valor not in ('01', '02', '03', '04', '50', '51', '52', '53', '54', '55'):
+            return ''
+
         #
         # Define as tags baseado no código da situação tributária
         #
