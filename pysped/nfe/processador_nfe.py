3a225c6acf2e0eb657ea69ae2a3fcf10d6f9208f
Ari Caldeira <aristides.caldeira@tauga.com.br>
2017-02-21 17:10:29 -0300

    Corrigindo código de retorno do processamento do lote de eventos

--- pysped/nfe/processador_nfe.py
+++ pysped/nfe/processador_nfe.py
@@ -1120,7 +1120,7 @@ class ProcessadorNFe(object):
         if self.salvar_arquivos:
             nome_arq = caminho + unicode(envio.idLote.valor).strip().rjust(15, '0') + '-rec-' + tipo_evento
 
-            if resposta.cStat.valor != '129':
+            if resposta.cStat.valor != '128':
                 nome_arq += '-rej.xml'
             else:
                 nome_arq += '.xml'
